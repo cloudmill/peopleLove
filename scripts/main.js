a6303cff16308560eeec9d9ac735b386ff1b9f56
Unknown <info@cloudmill.ru>
2019-01-04 23:02:01 +0200

    up

--- scripts/main.js
+++ scripts/main.js
@@ -369,7 +369,7 @@ $(window).on("load", function () {
 	setTimeout(function () {
 		$('.preloader-wrapper').fadeOut();
 		$('body').removeClass('preloader-site');
-	}, 1000);
+	}, 2000);
 });
 
 function initMap() {
